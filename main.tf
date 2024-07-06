7509151ba442641b8178ba862d33f89ceb826a12
aryanvijayvargiya <aryanvijayvargiya@outlook.in>
2024-07-06 15:16:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,22 +17,51 @@ output "subnet_ids" {
   value = aws_subnet.private_subnets[*].id
 }
 
+resource "null_resource" "install_layer_dependencies" {
+  provisioner "local-exec" {
+    command = "python3 -m pip install -r requirements.txt "
+  }
+  triggers = {
+    trigger = timestamp()
+  }
+}
+
+data "archive_file" "layer_zip" {
+  type        = "zip"
+  source_dir  = "layer"
+  output_path = "layer.zip"
+  depends_on = [
+    null_resource.install_layer_dependencies
+  ]
+}
+
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename = "layer.zip"
+  source_code_hash = data.archive_file.layer_zip.output_base64sha256
+  layer_name = "devops_exam_layer"
+
+  compatible_runtimes = ["python3.12"]
+  depends_on = [
+    data.archive_file.layer_zip
+  ]
+}
+
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "lambda_function.py"
   output_path = "lambda_function.zip"
 }
 
-provider "external" {
-  version = "~> 2.0"
-}
-
-resource "external_data_source" "example" {
-  program = ["python3.12", "./script.py"]
-}
-
 resource "aws_lambda_function" "lambda" {
   filename      = "lambda_function.zip"
+  source_code_hash = data.archive_file.functip_zip.output_base64sha256
+  layers = [
+    aws_lambda_layer_version.lambda_layer.arn
+  ]
+  depends_on = [
+    data.archive_file.function_zip,
+    aws_lambda_layer_version.lambda_layer
+  ]
   function_name = "DevOpsExamLambdaFunction"
   handler       = "lambda_function.lambda_handler"
   role          = data.aws_iam_role.lambda.arn
