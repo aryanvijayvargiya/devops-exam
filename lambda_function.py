37167d0659e1333aac753637093257960913213a
aryanvijayvargiya <aryanvijayvargiya@outlook.in>
2024-07-06 14:04:59 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,9 +4,9 @@ import requests
 
 def lambda_handler(event, context):
     # Replace with your actual values
-    subnet_id = event.get("subnet_id", "")
-    name = event.get("name", "")
-    email = event.get("email", "")
+    subnet_id = 'subnet-0009622cf3792440a'
+    name = 'Aryan Vijayvargiya'
+    email = 'aryanvijayvargiya16@gmail.com'
 
     # URL of the remote API endpoint
     url = "https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data"
