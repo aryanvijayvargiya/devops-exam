fe094005b8ae304d2d10f976880bd7166b560edd
aryanvijayvargiya <aryanvijayvargiya@outlook.in>
2024-07-06 13:49:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,29 +23,6 @@ data "archive_file" "lambda" {
   output_path = "lambda_function.zip"
 }
 
-resource "aws_security_group" "lambda_sg" {
-  name        = "lambdasecuritygroup"
-  description = "Security group for Lambda function"
-
-  vpc_id = data.aws_vpc.vpc.id 
-  // Allow inbound traffic
-  ingress {
-    description = "Allow HTTP inbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // Allow outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_lambda_function" "lambda" {
   filename      = "lambda_function.zip"
   function_name = "DevOpsExamLambdaFunction"
