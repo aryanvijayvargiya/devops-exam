7561f110597ca0deb196aec0b2944aabc9e5d741
aryanvijayvargiya <aryanvijayvargiya@outlook.in>
2024-07-06 12:32:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    S3 Bucket         = "467.devops.candidate.exam"
-    Key            = "Aryan.Vijayvargiya"
-    Region         = "ap-south-1"
+        S3 Bucket: "467.devops.candidate.exam"
+        Region: "ap-south-1"
+        Key: "Aryan.Vijayvargiya"
   }
 }
