3e34e58ffa82477015347517ad95a2b5dc135c28
aryanvijayvargiya <aryanvijayvargiya@outlook.in>
2024-07-06 13:17:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,20 +13,6 @@ resource "aws_subnet" "private_subnets" {
   cidr_block        = var.subnet_cidrs[count.index]
 }
 
-resource "aws_route_table" "route_table_private" {
-  vpc_id = data.aws_vpc.vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = data.aws_nat_gateway.nat.id
-  }
-}
-
-resource "aws_route_table_association" "route_table_association_private" {
-  subnet_id      = aws_subnet.private_subnets[0].id
-  route_table_id = aws_route_table.route_table_private.id
-}
-
 output "subnet_ids" {
   value = aws_subnet.private_subnets[*].id
 }
@@ -35,29 +21,6 @@ data "archive_file" "lambda" {
   type        = "zip"
   source_file = "lambda_function.py"
   output_path = "lambda_function.zip"
-}
-
-resource "aws_security_group" "lambda_sg" {
-  name        = "lambda-security-group"
-  description = "Security group for Lambda function"
-
-  vpc_id = data.aws_vpc.vpc.id 
-  // Allow inbound traffic
-  ingress {
-    description = "Allow HTTP inbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // Allow outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 
@@ -69,7 +32,6 @@ resource "aws_lambda_function" "lambda" {
   runtime       = "python3.12"
 
   vpc_config {
-    subnet_ids         = [aws_subnet.private_subnets[0].id]  # Replace with your subnet IDs
-    security_group_ids = [aws_security_group.lambda_sg.id]   # Replace with your security group ID
+    subnet_ids         = [aws_subnet.private_subnets[0].id]
   }
 }
